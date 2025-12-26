e7344fe5b8ef0380bd1dfc111299adeb23ad144a
LucaLzt <luca.lazarte05@gmail.com>
2025-12-26 20:20:55 -0300

    refactor(error-handler): 'type' is added to comply with the ProblemDetail standard

--- src/main/java/com/ejemplos/jwt/infrastructure/web/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ejemplos/jwt/infrastructure/web/exception/GlobalExceptionHandler.java
@@ -12,13 +12,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.URI;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String ERROR_URI_BASE = "https://api.midominio.com/errors/";
 
     @ExceptionHandler(BadRequestException.class)
     public ProblemDetail handleBadRequest(BadRequestException e) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
         problem.setTitle("Bad Request");
+        problem.setType(URI.create(ERROR_URI_BASE + "bad-request"));
         return problem;
     }
 
@@ -26,6 +31,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleResourceNotFound(ResourceNotFound e) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
         problem.setTitle("Resource Not Found");
+        problem.setType(URI.create(ERROR_URI_BASE + "resource-not-found"));
         return problem;
     }
 
@@ -33,6 +39,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleConflict(ConflictException e) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
         problem.setTitle("Resource Conflict");
+        problem.setType(URI.create(ERROR_URI_BASE + "resource-conflict"));
         return problem;
     }
 
@@ -40,6 +47,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleUnauthorized(UnauthorizedException e) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
         problem.setTitle("Unauthorized");
+        problem.setType(URI.create(ERROR_URI_BASE + "unauthorized"));
         return problem;
     }
 
@@ -47,6 +55,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleDomainException(BadCredentialsException e) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, "Invalid email or password");
         problem.setTitle("Unauthorized");
+        problem.setType(URI.create(ERROR_URI_BASE + "unauthorized"));
         return problem;
     }
 
@@ -54,6 +63,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleSecurityBreach(SecurityBreachException e) {
         ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
         problem.setTitle("Security Breach Detected");
+        problem.setType(URI.create(ERROR_URI_BASE + "security-breach"));
         return problem;
     }
 }
