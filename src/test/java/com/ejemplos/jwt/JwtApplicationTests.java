5d865224763ce1c037741b83d290a2e0c84572fe
LucaLzt <luca.lazarte05@gmail.com>
2026-01-10 22:30:50 -0300

    feat(test): Add integration testing infrastructure with Testcontainers

--- src/test/java/com/ejemplos/jwt/JwtApplicationTests.java
+++ src/test/java/com/ejemplos/jwt/JwtApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JwtApplicationTests {
+class JwtApplicationTests extends AbstractIT {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
